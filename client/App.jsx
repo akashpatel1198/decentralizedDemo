8ecb50f21a756c384a00a76df48bbe13115cf403
akash patel <akash.vinay.patel@gmail.com>
2023-01-22 13:53:34 -0800

    configured and added basic tailwindCSS and tsParticles

--- client/App.jsx
+++ client/App.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import Navbar from "./components/Navbar.jsx";
 import { NavContextProvider } from "./contexts/NavContext.jsx";
 import MainContainer from "./containers/MainContainer.jsx";
+import ParticlesComponent from "./components/Particles.jsx";
 
 const App = () => {
+
   return(
-    <div>
+    <div className="flex flex-col items-center">
     <NavContextProvider>
       <Navbar></Navbar>
       <MainContainer></MainContainer>
+      <ParticlesComponent
+      ></ParticlesComponent>
     </NavContextProvider>
     </div>
   );
